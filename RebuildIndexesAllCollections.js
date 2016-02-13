406bfbd69dfe665d3944fbc9e285bcfe4dd52504
Chad England <chadit@gmail.com>
2016-02-13 13:15:51 -0500

    added index rebuild for all collections

---
+++ RebuildIndexesAllCollections.js
@@ -0,0 +1,7 @@
+print (db)
+var collections = db.getCollectionNames().sort();
+var collectionCount = collections.length;
+for (i = 0; i < collectionCount; i++) { 
+    print(collections[i] + " rebuilding indexes ");
+    db[collections[i]].reIndex();
+}
